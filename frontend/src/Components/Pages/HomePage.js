a701685e1babcb5290c5f4942844450b4b2462c4
maxime.vanrechem@student.vinci.be <Ourson=2000>
2022-10-22 16:56:45 +0200

    HomepageTest

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,6 +1,20 @@
+import brainWallPaper from '../../img/wallPaperTest.png';
+
+
+const homePage = `
+<div class="text-center">
+  <h3>Welcome to Brainy !</h3>
+
+  <p>Here you can try to improve your brain level</p>
+  <div class="pb-3">
+    <img class="img-thumbnail w-50" src="${brainWallPaper}" alt="BrainWallpaper" />
+  </div>
+</div>`;
+
+
 const HomePage = () => {
   const main = document.querySelector('main');
-  main.innerHTML = 'Deal with the content of your HomePage';
+  main.innerHTML = homePage;
 };
 
 export default HomePage;
