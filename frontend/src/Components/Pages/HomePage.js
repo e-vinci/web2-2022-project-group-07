fd9dc88584c61ce59a7d2f973025c169b7be34ae
Sacha Norden <sacha.norden@student.vinci.be>
2022-11-07 11:08:37 +0100

    text responsive

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -3,24 +3,21 @@ import rotatingBrain from '../../img/rotatingBrain.gif';
 
 
 const homePage = `
-<div class="container-fluid">
+
   <div class="row">
     <section class="padding-x-md padding-y-lg">
         <div class="grid gap-md items-center">
-            
+                <div class="gif">
+                    <img class="img-fluid" src="${rotatingBrain}" alt="Responsive image" />
+                </div>
                 <div class="float-lg-start text-component">
                     <h1>Welcome to BeBrain !</h1>
                     <p>Here you can try to improve your brain level and discover his potential.</p>
                 </div>
-            
-            
-                <div class="gif">
-                    <img class="img-fluid" src="${rotatingBrain}" alt="Responsive image" />
-                </div>
         </div>
     </section>
   </div>
-</div>`;
+`;
 
 
 const HomePage = () => {
