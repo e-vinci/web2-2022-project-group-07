d726547d4cf06eace9687a07a46bdb8df5ca752a
maxime.vanrechem@student.vinci.be <Ourson=2000>
2022-11-13 16:13:45 +0100

    Persona

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -4,6 +4,7 @@ import NewPage from '../Pages/NewPage';
 import LoginPageOldVersion from '../Pages/LoginPageOldVersion';
 import LoginPage from '../Pages/LoginPage';
 import RegisterPage from '../Pages/RegisterPage';
+import ReflexePage from '../Pages/ReflexePage';
 
 const routes = {
   '/': HomePage,
@@ -12,6 +13,7 @@ const routes = {
   '/loginOldVersion' : LoginPageOldVersion,
   '/login' : LoginPage,
   '/register': RegisterPage,
+  '/reflexePage': ReflexePage,
 };
 
 export default routes;
