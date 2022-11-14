bde0d410c5705fe224f4992fc25d93d2c01f21f3
paul.vandecasteele <paul.vandecasteele@student.vinci.be>
2022-11-14 11:30:46 +0100

    Add animed backGround

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -4,7 +4,7 @@ import NewPage from '../Pages/NewPage';
 import LoginPageOldVersion from '../Pages/LoginPageOldVersion';
 import LoginPage from '../Pages/LoginPage';
 import RegisterPage from '../Pages/RegisterPage';
-import ReflexePage from '../Pages/ReflexePage';
+
 
 const routes = {
   '/': HomePage,
@@ -13,7 +13,6 @@ const routes = {
   '/loginOldVersion' : LoginPageOldVersion,
   '/login' : LoginPage,
   '/register': RegisterPage,
-  '/reflexePage': ReflexePage,
 };
 
 export default routes;
