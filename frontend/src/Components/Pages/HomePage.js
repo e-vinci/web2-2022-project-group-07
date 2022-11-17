344d736c4860fa9e10d7244310b343c47efa4c39
Sacha Norden <sacha.norden@student.vinci.be>
2022-11-17 12:45:02 +0100

    ajout de boutons et apparait

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,5 +1,6 @@
 // import brainWallPaper from '../../img/wallPaperTest.png';
 import rotatingBrain from '../../img/rotatingBrain.gif';
+import gamebutton from '../../img/verte.png';
 
 
 const homePage = `
@@ -7,13 +8,13 @@ const homePage = `
   <div class="row">
     <section class="padding-x-md padding-y-lg">
         <div class="grid gap-md items-center">
-                <div class="gif">
-                    <img class="img-fluid" src="${rotatingBrain}" alt="Responsive image" />
-                </div>
-                <div class="float-lg-start text-component">
-                    <h1>Welcome to BeBrain !</h1>
-                    <p>Here you can try to improve your brain level and discover his potential.</p>
-                </div>
+            <div id='hide' class="gif">
+                <img id ='1' class="img-fluid" src="${rotatingBrain}" alt="Responsive image" />
+            </div>
+            <div class="float-lg-start text-component">
+                <h1>Welcome to BeBrain !</h1>
+                <p>Here you can try to improve your brain level and discover his potential.</p>
+            </div>
         </div>
     </section>
   </div>
@@ -23,6 +24,21 @@ const homePage = `
 const HomePage = () => {
     const main = document.querySelector('main');
     main.innerHTML = homePage;
+    const games=document.getElementById('hide');
+    let isclicked =false;
+    games.addEventListener('click',()=>{
+        if(!isclicked) {
+            games.innerHTML += `
+            <img class='reflexgame ' src="${gamebutton}" alt="button game" className="img-fluid">
+            <img class='reflexgame ' src="${gamebutton}" alt="button game" className="img-fluid">
+            <img class='reflexgame ' src="${gamebutton}" alt="button game" className="img-fluid">
+            <img class='reflexgame ' src="${gamebutton}" alt="button game" className="img-fluid">
+            
+            `;
+        }
+        isclicked=true;
+
+    });
 };
 
 export default HomePage;
