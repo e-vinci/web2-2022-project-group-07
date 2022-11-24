d9d205a61c86546e8f708940b3e3b1430a03d7a6
maxime.vanrechem@student.vinci.be <Ourson=2000>
2022-11-24 14:06:15 +0100

    route correction

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -1,7 +1,6 @@
 import GamePage from '../Pages/GamePage';
 import HomePage from '../Pages/HomePage';
 import NewPage from '../Pages/NewPage';
-import LoginPageOldVersion from '../Pages/LoginPageOldVersion';
 import LoginPage from '../Pages/LoginPage';
 import RegisterPage from '../Pages/RegisterPage';
 import ReflexGame from '../Pages/ReflexGame';
@@ -12,7 +11,6 @@ const routes = {
   '/': HomePage,
   '/game': GamePage,
   '/new': NewPage,
-  '/loginOldVersion' : LoginPageOldVersion,
   '/login' : LoginPage,
   '/register': RegisterPage,
   '/reflexPage' : ReflexGame,
