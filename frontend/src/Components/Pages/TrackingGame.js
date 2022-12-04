e9c2560253a2efde32a3b650813ee5c3e2a0b5ba
Sacha Norden <sacha.norden@student.vinci.be>
2022-12-04 14:37:31 +0100

    make the background and can detect the cursor mouse

--- frontend/src/Components/Pages/TrackingGame.js
+++ frontend/src/Components/Pages/TrackingGame.js
@@ -1,59 +1,36 @@
 import Phaser from 'phaser';
-import {clearPage, renderPageTitle} from "../../utils/render";
-import trackingBackground from '../../img/trackingBackground.jpg';
-
+import GameScene from "../Game/GameScene";
 
 let game;
-const config = {
-  type: Phaser.AUTO,
-  width: 1200,
-  height: 500,
-  physics: {
-    default: 'arcade',
-    arcade: {
-        gravity: {y: 0},
-        debug: false
-    }
-  },
-    scene: {
-        preload: preload(),
-        create: create(),
-        update: update()
-    }
-};
-
 
 const TrackingGame = () => {
-  clearPage();
-  renderPageTitle('Tracking Game');
-  renderTrackingGame()
-  if (game) game.destroy(true);
-  game = new Phaser.Game(config);
-  game.load.image('background', trackingBackground);
-  game.add.image(537, 322, 'background');
-};
-function preload() {
-
-}
-function create() {
-
-}
-
-function update() {
-
-}
-
-function renderTrackingGame() {
-  const main = document.querySelector('main');
-  main.innerHTML = `
+    const trackingGame = `
 <div class="container-fluid">
-    <section class="padding-x-md padding-y-lg">
         <div id="gameDiv" class="centerPageTracking d-flex justify-content-center my-3">
         </div>
-    </section>
-  
-</div>
-  `;
-}
+</div>`;
+    const main = document.querySelector('main');
+    main.innerHTML = trackingGame;
+    const config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 500,
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 300 },
+                debug: false,
+            },
+        },
+        scene: [GameScene],
+        //  parent DOM element into which the canvas created by the renderer will be injected.
+        parent: 'gameDiv',
+    };
+
+    // there could be issues when a game was quit (events no longer working)
+    // therefore destroy any started game prior to recreate it
+    if (game) game.destroy(true);
+    game = new Phaser.Game(config);
+};
 
 export default TrackingGame;
