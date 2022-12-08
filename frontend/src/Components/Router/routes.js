d081e23939bb655029efa6f8123d984a4de7a6ee
maxime.vanrechem@student.vinci.be <Ourson=2000>
2022-12-08 12:44:35 +0100

    logout

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -1,21 +1,21 @@
-import GamePage from '../Pages/GamePage';
 import HomePage from '../Pages/HomePage';
 import NewPage from '../Pages/NewPage';
 import LoginPage from '../Pages/LoginPage';
 import RegisterPage from '../Pages/RegisterPage';
 import ReflexGame from '../Pages/ReflexGame';
 import FastClickGame from '../Pages/FastClickGame';
+import Logout from '../Logout/Logout';
 
 
 
 const routes = {
   '/': HomePage,
-  '/game': GamePage,
   '/new': NewPage,
   '/login' : LoginPage,
   '/register': RegisterPage,
   '/reflexPage' : ReflexGame,
   '/fastClick' : FastClickGame,
+  '/logout': Logout,
 };
 
 export default routes;
