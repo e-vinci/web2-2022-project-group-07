971e7f26934f8bb0897b556efd6c6e347e914605
SachaNorden <sacha.norden@student.vinci.be>
2022-12-13 22:56:16 +0100

    score set up and detect mouse inside the circle

--- frontend/src/Components/Game/GameScene.js
+++ frontend/src/Components/Game/GameScene.js
@@ -5,9 +5,8 @@ import backgroundSky from '../../img/trackingBackground.jpg';
 import bombAsset from '../../assets/bomb.png';
 import trackingButton from '../../img/tracking_button.png';
 
-
 const BOMB_KEY = 'bomb';
-let cursors;
+
 let circle;
 
 class GameScene extends Phaser.Scene {
@@ -25,15 +24,17 @@ class GameScene extends Phaser.Scene {
     this.load.image(BOMB_KEY, bombAsset);
     this.load.image('trackButton', trackingButton);
 
+
   }
 
   create() {
     this.add.image(400, 300, 'sky').setScale(2);
     this.scoreLabel = this.createScoreLabel(16, 16, 0);
     this.matter.world.setBounds().disableGravity();
-   // let test = this.add.sprite(180,400, 'trackButton').setScale(0.1);
 
-    circle =this.matter.add.image(10, 30, 'trackButton');
+   // let test = this.add.sprite(180,400, 'trackButton').setScale(0.1);
+    circle =this.matter.add.image(10, 30, 'trackButton'); // initial position of the circle
+    // this.scene.pause();
     circle.setCircle(640);
     circle.setScale(0.08);
 
@@ -41,16 +42,7 @@ class GameScene extends Phaser.Scene {
     circle.setBounce(1.001);
     circle.setFriction(0, 0, 10);
 
-
     // player
-
-
-
-
-
-
-
-
     /* The Collider takes two objects and tests for collision and performs separation against them.
     Note that we could call a callback in case of collision... */
   }
@@ -59,15 +51,27 @@ class GameScene extends Phaser.Scene {
     if (this.gameOver) {
       return;
     }
+    const xPosition=circle.x;
+    const yPosition=circle.y;
+
+    if(this.input.mousePointer.position.x<=800 && this.input.mousePointer.position.y<=500 && this.input.mousePointer.position.x>0 && this.input.mousePointer.position.y>0){
+      // console.log(this.input.mousePointer.x, this.input.mousePointer.y);
+      if(this.input.mousePointer.x>xPosition-50 && this.input.mousePointer.x<xPosition+50 && this.input.mousePointer.y>yPosition-50 && this.input.mousePointer.y<yPosition+50 ){
+        this.scoreLabel.add(1);
+      }
+      else{
+        this.scoreLabel.add(-1);
+      }
+
+    }
+
 
     // circle.body.velocity.x=(Phaser.Math.RandomXY(cursors, 100).x);
     // circle.body.velocity.y=(Phaser.Math.RandomXY(cursors, 200).y);
     // circle.body.deltaMax.x = Phaser.Math.Between(0, 100);
 
-   console.log(this.matter.bodyBounds.getCenter(circle));
+
   }
-
-
 
 
   createScoreLabel(x, y, score) {
