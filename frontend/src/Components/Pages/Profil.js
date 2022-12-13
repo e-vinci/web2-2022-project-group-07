a7c84dd15124893436d9ec447ecfc8696c4f746c
maxime.vanrechem@student.vinci.be <Ourson=2000>
2022-12-13 11:36:42 +0100

    Profil frontend

--- frontend/src/Components/Pages/Profil.js
+++ frontend/src/Components/Pages/Profil.js
@@ -1,8 +1,10 @@
-import { getRememberMe, setAuthenticatedUser, setRememberMe } from '../../utils/auths';
 import { clearPage, renderPageTitle } from '../../utils/render';
 import profilBrain from '../../img/profilBrain.png';
+import { getAuthenticatedUser } from '../../utils/auths';
+/*
 import Navbar from '../Navbar/Navbar';
 import Navigate from '../Router/Navigate';
+*/
 
 const ProfilPage = () => {
   clearPage();
@@ -17,11 +19,12 @@ function renderProfilForm() {
     container.className = 'container d-flex justify-content-center';
 
     const card = document.createElement('div');
-    container.className = 'card p-3 py-4';
+    card.className = 'card p-3 py-4';
+    card.id = 'cardProfil';
     container.appendChild(card);
 
     const text = document.createElement('div');
-    container.className = 'text-center';
+    text.className = 'text-center';
     card.appendChild(text);
 
     const image = document.createElement('img');
@@ -32,5 +35,42 @@ function renderProfilForm() {
 
     const nom = document.createElement('h3');
     nom.className = 'mt-2';
-    nom.textContent = "Mon Nom";
+    nom.textContent = "My Username";
     text.appendChild(nom);
+
+    const row = document.createElement('div');
+    row.className = 'row mt-3 mb-3';
+
+    const game1 = document.createElement('div');
+    game1.className = 'col-md-4';
+    const title1 = document.createElement('h5');
+    title1.textContent = 'Reflexe Game';
+    game1.appendChild(title1);
+    const num1 = document.createElement('span');
+    num1.className = 'num';
+    num1.textContent = 10;
+    game1.appendChild(num1);
+    row.appendChild(game1);
+
+
+
+    text.appendChild(row);
+    main.appendChild(container);
+}
+
+/*
+
+const readUsername = async () => {
+  const userSession = getAuthenticatedUser();
+  userSession.
+  const response = await fetch(`api/user/username`);
+
+  if (!response.ok) {
+    throw new Error(`readUsername:: fetch error : ${response.status} : ${response.statusText}`);
+  }
+  const user = await response.json();
+  return user.username;
+
+};
+*/
+export default ProfilPage;
