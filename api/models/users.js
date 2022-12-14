576f5df7f7f6fec8769ecae27834dde8b7fb61fb
maxence.vangrunderbeec <maxence.vangrunderbeec@student.vinci.be>
2022-12-14 16:11:48 +0100

    update scores (not 100% completed)

--- api/models/users.js
+++ api/models/users.js
@@ -1,110 +1,129 @@
-"use strict"
+'use strict';
 
-const jwt = require("jsonwebtoken");
-const jwtSecret = "maxouBebouLePlusBo";
+const jwt = require('jsonwebtoken');
+const jwtSecret = 'maxouBebouLePlusBo';
 const LIFETIME_JWT = 24 * 60 * 60 * 1000; // in ms : 24 * 60 * 60 * 1000 = 24h
 
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-const { parse, serialize} = require("../utils/json");
-const jsonDbPath = __dirname + "/../data/users.json";
+const { parse, serialize } = require('../utils/json');
+const jsonDbPath = __dirname + '/../data/users.json';
 
 const defaultItems = [
-    {
-        id: 1,
-        username: "admin",
-        password: "$2b$10$RqcgWQT/Irt9MQC8UfHmjuGCrQkQNeNcU6UtZURdSB/fyt6bMWARa",
-    },
+  {
+    id: 1,
+    username: 'admin',
+    password: '$2b$10$RqcgWQT/Irt9MQC8UfHmjuGCrQkQNeNcU6UtZURdSB/fyt6bMWARa',
+    scoreReflexe: 0,
+    scoreFastClick: 0,
+    scoreTracking: 0,
+    scoreMemory: 0,
+  },
 ];
 
 class Users {
+  constructor(dbPath = jsonDbPath, items = defaultItems) {
+    this.jsonDbPath = dbPath;
+    this.defaultItems = items;
+    serialize(this.jsonDbPath, this.defaultItems);
+  }
+
+  getNextId() {
+    const users = parse(this.jsonDbPath);
+    const lastItemIndex = users?.length !== 0 ? users.length - 1 : undefined;
+    if (lastItemIndex === undefined) return 1;
+    const lastId = users[lastItemIndex]?.id;
+    const nextId = lastId + 1;
+    return nextId;
+  }
+
+  getOneById(id) {
+    const items = parse(this.jsonDbPath);
+    const foundIndex = items.findIndex((item) => item.id == id);
+    if (foundIndex < 0) return;
+
+    return items[foundIndex];
+  }
+
+  getOneByUsername(username) {
+    const items = parse(this.jsonDbPath);
+    const foundIndex = items.findIndex((item) => item.username == username);
+    if (foundIndex < 0) return;
+
+    return items[foundIndex];
+  }
+
+  updateOne(username, body) {
+    const items = parse(this.jsonDbPath);
+    const foundIndex = items.findIndex((item) => item.username == username);
+    if (foundIndex < 0) return;
+
+    const updateditem = { ...items[foundIndex], ...body };
     
-    constructor(dbPath = jsonDbPath, items = defaultItems){
-        this.jsonDbPath = dbPath;
-        this.defaultItems = items;
-        serialize(this.jsonDbPath, this.defaultItems);
-    }
+    items[foundIndex] = updateditem;
 
-    getNextId(){
-        const items = parse(this.jsonDbPath);
-        let nextId;
-        if(items.length === 0)nextId = 1;
-        else nextId = items[items.length-1].id + 1;
-    }
+    serialize(this.jsonDbPath, items);
+    return updateditem;
+  }
 
-    getOneById(id){
-        const items = parse(this.jsonDbPath);
-        const foundIndex = items.findIndex((item) => item.id == id);
-        if(foundIndex < 0)return;
+  async addOne(body) {
+    const items = parse(jsonDbPath);
 
-        return items[foundIndex];
-    }
+    const hashedPassword = await bcrypt.hash(body.password, saltRounds);
 
-    getOneByUsername(username){
-        const items = parse(this.jsonDbPath);
-        const foundIndex = items.findIndex((item) => item.username == username);
-        if(foundIndex < 0)return;
+    const newItem = {
+      id: this.getNextId(),
+      username: body.username,
+      password: hashedPassword,
+      scoreReflexe: 0,
+      scoreFastClick: 0,
+      scoreTracking: 0,
+      scoreMemory: 0,
+    };
+    items.push(newItem);
+    serialize(this.jsonDbPath, items);
+    return newItem;
+  }
 
-        return items[foundIndex];
-    }
+  async login(username, password) {
+    const userFound = this.getOneByUsername(username);
+    if (!userFound) return;
 
-    async addOne(body){
-        const items = parse(jsonDbPath);
+    const matchPW = await bcrypt.compare(password, userFound.password);
+    if (!matchPW) return;
 
-        const hashedPassword = await bcrypt.hash(body.password, saltRounds);
+    const authenticatedUser = {
+      username: username,
+      token: 'Future signed token',
+    };
 
-        const newItem = {
-            username: body.username,
-            password: hashedPassword,
-        };
-        items.push(newItem);
-        serialize(this.jsonDbPath, items);
-        return newItem;
-    }
+    const token = jwt.sign({ username: authenticatedUser.username }, jwtSecret, {
+      expiresIn: LIFETIME_JWT,
+    });
 
-    async login(username, password){
-        const userFound = this.getOneByUsername(username);
-        if(!userFound)return;
+    authenticatedUser.token = token;
+    return authenticatedUser;
+  }
 
-        const matchPW = await bcrypt.compare(password, userFound.password);
-        if(!matchPW)return;
+  register(username, password) {
+    const userFound = this.getOneByUsername(username);
+    if (userFound) return;
 
-        const authenticatedUser = {
-            username: username,
-            token: "Future signed token",
-        };
+    const newUser = this.addOne({ username: username, password: password });
 
-        const token = jwt.sign(
-            { username: authenticatedUser.username },
-            jwtSecret,
-            { expiresIn: LIFETIME_JWT },
-        );
+    const authenticatedUser = {
+      username: username,
+      token: 'Future signed token',
+    };
 
-        authenticatedUser.token = token;
-        return authenticatedUser;
-    }
+    const token = jwt.sign({ username: authenticatedUser.username }, jwtSecret, {
+      expiresIn: LIFETIME_JWT,
+    });
 
-    register(username, password){
-        const userFound = this.getOneByUsername(username);
-        if(userFound)return;
-
-        const newUser = this.addOne({username: username, password: password});
-
-        const authenticatedUser = {
-            username: username,
-            token: "Future signed token",
-        };
-
-        const token = jwt.sign(
-            { username: authenticatedUser.username },
-            jwtSecret,
-            { expiresIn: LIFETIME_JWT },
-        );
-
-        authenticatedUser.token = token;
-        return authenticatedUser; 
-    }
+    authenticatedUser.token = token;
+    return authenticatedUser;
+  }
 }
 
 module.exports = { Users };
---
+++ api/routes/userScores.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const { Users } = require('../models/users');
+
+const router = express.Router();
+const userModel = new Users();
+
+router.put('/scoreReflexe', async function(req, res, next) {
+  if (!req.body || (req.body.hasOwnProperty('time-text') && req.body.scoreReflexe === ''))
+    return res.status(400).end();
+  /* continue change for reflexe game*/
+
+  const updatedUser = await userModel.updateOne(req.body.username, req.body);
+
+  if (!updatedUser) return res.status(409).end();
+
+  return res.json(updatedUser);
+});
+
+/*DEMANDER AU PROF SI JE PEUX VERIFIER LE SCORE ICI (ANCIEN SCORE <= NOUVEAU SCORE) */
+router.put('/scoreFastClick', async function(req, res, next) {
+  if (!req.body || (req.body.hasOwnProperty('time-text') && req.body.score === ''))
+    return res.status(400).end();
+  /* continue change for faskclick game*/
+
+  const updatedUser = await userModel.updateOne(req.body.username, req.body);
+
+  if (!updatedUser) return res.status(409).end();
+
+  return res.json(updatedUser);
+});
+
+module.exports = router;
+
+/*router.post('/scoreAimClick', async function (req, res, next) {
+    if (
+      !req.body ||
+      (req.body.hasOwnProperty('time-text') && req.body.score === '')
+    )
+      return res.status(400).end();
+      const authenticatedUser = await userModel.register(req.body.username, req.body.password);
+  
+      if(!authenticatedUser)return res.status(409).end();
+  
+      return res.json(authenticatedUser);
+  });*/
+
+/*router.post('/scoreTracking', async function (req, res, next) {
+    if (
+      !req.body ||
+      (req.body.hasOwnProperty('time-text') && req.body.score === '')
+    )
+      return res.status(400).end();
+       const authenticatedUser = await userModel.register(req.body.username, req.body.password);
+  
+      if(!authenticatedUser)return res.status(409).end();
+  
+      return res.json(authenticatedUser);
+  });*/
